88ad3f7997ed3a739d2ffd66c43adb1c078e26e7
CarlosVasconcellos <44424001+CarlosVasconcellos@users.noreply.github.com>
2019-11-09 23:40:00 -0300

    close adicionado

--- Emotional1.0/src/br/com/emotional/persistencia/ConnectionFactory.java
+++ Emotional1.0/src/br/com/emotional/persistencia/ConnectionFactory.java
@@ -9,21 +9,36 @@ public class ConnectionFactory {
 	private final String URL = "jdbc:mysql://localhost:3306/Emotional_BD?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false";
 	private final String USER = "root";
 	private final String PASS = "admin";
-	
+	Connection con;
+	PreparedStatement stmt;
+	ResultSet rs;
 	
 	/*
     public Connection getConnection() throws Exception {       
     		return DriverManager.getConnection(URL,USER,PASS);
       
     }
-    */
-    
-    public Connection getConnection() {
+    */  
+    public Connection getConnection() throws Exception {
         try {
+    		Class.forName("com.mysql.jdbc.Driver");
             return DriverManager.getConnection(URL,USER,PASS);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
     
+    
+	public void close()throws Exception {
+		System.out.println("closing");
+		if(rs != null)
+			rs.close();
+		
+		if(stmt != null)
+			stmt.close();
+		
+		if(con != null)
+			con.close();
+		System.out.println("closed");
+	}
 }
